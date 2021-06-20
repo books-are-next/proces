6f1c31941596d694c112b3b6b7390d2061d9ac15
evavichova <85667482+evavichova@users.noreply.github.com>
2021-06-20 13:36:05 +0000

    Deploying to docs from @ books-are-next/proces@108e4328f85677f3a59f4ef0e70dc8f20bafcd07 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* global self, caches, fetch */
 /* eslint-disable no-restricted-globals */
 
-const CACHE = 'cache-3cec03e';
+const CACHE = 'cache-108e432';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
@@ -39,7 +39,7 @@ self.addEventListener('activate', event => {
 });
 
 function precache() {
-  return caches.open(CACHE).then(cache => cache.addAll(["./","./favicon.png","./index.html","./manifest.json","./proces_003.html","./proces_004.html","./proces_005.html","./proces_006.html","./proces_007.html","./proces_008.html","./proces_009.html","./proces_010.html","./proces_011.html","./proces_012.html","./proces_013.html","./proces_014.html","./proces_015.html","./proces_016.html","./proces_017.html","./proces_019.html","./proces_018.html","./proces_020.html","./proces_021.html","./proces_022.html","./proces_023.html","./proces_024.html","./proces_025.html","./proces_026.html","./scripts/bundle.js","./style/style.min.css"]));
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./favicon.png","./index.html","./manifest.json","./proces_003.html","./proces_004.html","./proces_005.html","./proces_006.html","./proces_007.html","./proces_008.html","./proces_009.html","./proces_010.html","./proces_011.html","./proces_012.html","./proces_013.html","./proces_014.html","./proces_015.html","./proces_016.html","./proces_017.html","./proces_018.html","./proces_019.html","./proces_020.html","./proces_021.html","./proces_022.html","./proces_023.html","./proces_024.html","./proces_025.html","./proces_026.html","./scripts/bundle.js","./style/style.min.css"]));
 }
 
 self.addEventListener('fetch', e => {
